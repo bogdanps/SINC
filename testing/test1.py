5c31e73b69c86f997293fb9fbbeea0d1e06e087c
Bogdan Serban <vartic112@gmail.com>
2023-01-17 12:25:36 +0200

    Testing job stage

--- testing/test1.py
+++ testing/test1.py
@@ -1,10 +1,16 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-driver = webdriver.Chrome()
-driver.maximize_window()
+from selenium.webdriver.chrome.options import Options
 
-driver.get("http://localhost:3000")
+chrome_options = Options()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get("https://sinc.herokuapp.com/")
 
 class AuthTest:
     def __init__(self):
